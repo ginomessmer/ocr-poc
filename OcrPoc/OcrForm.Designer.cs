ea1ed32c353ef1873081b916940d6f3db5bfc2f2
Gino Messmer <g.messmer@ftsoftware.eu>
2017-10-05 15:30:59 +0200

    Support for tessdata files

--- OcrPoc/OcrForm.Designer.cs
+++ OcrPoc/OcrForm.Designer.cs
@@ -28,58 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.imgInput = new System.Windows.Forms.PictureBox();
+            this.cmdSelectImage = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtTessdataDir = new System.Windows.Forms.TextBox();
+            this.cmdBrowseTessdataDir = new System.Windows.Forms.Button();
+            this.txtLanguage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgInput)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // imgInput
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(237, 237);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.imgInput.Location = new System.Drawing.Point(6, 19);
+            this.imgInput.Name = "imgInput";
+            this.imgInput.Size = new System.Drawing.Size(237, 237);
+            this.imgInput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgInput.TabIndex = 0;
+            this.imgInput.TabStop = false;
             // 
-            // button1
+            // cmdSelectImage
             // 
-            this.button1.Location = new System.Drawing.Point(12, 226);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(237, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Select image";
-            this.button1.UseVisualStyleBackColor = true;
+            this.cmdSelectImage.Location = new System.Drawing.Point(6, 262);
+            this.cmdSelectImage.Name = "cmdSelectImage";
+            this.cmdSelectImage.Size = new System.Drawing.Size(467, 23);
+            this.cmdSelectImage.TabIndex = 1;
+            this.cmdSelectImage.Text = "Select image";
+            this.cmdSelectImage.UseVisualStyleBackColor = true;
+            this.cmdSelectImage.Click += new System.EventHandler(this.cmdSelectImage_Click);
             // 
-            // textBox1
+            // txtOutput
             // 
-            this.textBox1.Location = new System.Drawing.Point(255, 12);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(217, 237);
-            this.textBox1.TabIndex = 2;
+            this.txtOutput.Location = new System.Drawing.Point(249, 19);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(224, 237);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmdSelectImage);
+            this.groupBox1.Controls.Add(this.imgInput);
+            this.groupBox1.Controls.Add(this.txtOutput);
+            this.groupBox1.Location = new System.Drawing.Point(12, 101);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(479, 292);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Scan";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtLanguage);
+            this.groupBox2.Controls.Add(this.txtTessdataDir);
+            this.groupBox2.Controls.Add(this.cmdBrowseTessdataDir);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(479, 83);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Config";
+            // 
+            // txtTessdataDir
+            // 
+            this.txtTessdataDir.Location = new System.Drawing.Point(6, 19);
+            this.txtTessdataDir.Name = "txtTessdataDir";
+            this.txtTessdataDir.ReadOnly = true;
+            this.txtTessdataDir.Size = new System.Drawing.Size(467, 20);
+            this.txtTessdataDir.TabIndex = 2;
+            // 
+            // cmdBrowseTessdataDir
+            // 
+            this.cmdBrowseTessdataDir.Location = new System.Drawing.Point(310, 45);
+            this.cmdBrowseTessdataDir.Name = "cmdBrowseTessdataDir";
+            this.cmdBrowseTessdataDir.Size = new System.Drawing.Size(163, 23);
+            this.cmdBrowseTessdataDir.TabIndex = 1;
+            this.cmdBrowseTessdataDir.Text = "Browse *.traineddata file";
+            this.cmdBrowseTessdataDir.UseVisualStyleBackColor = true;
+            this.cmdBrowseTessdataDir.Click += new System.EventHandler(this.cmdBrowseTessdataDir_Click);
+            // 
+            // txtLanguage
+            // 
+            this.txtLanguage.Location = new System.Drawing.Point(89, 47);
+            this.txtLanguage.Name = "txtLanguage";
+            this.txtLanguage.ReadOnly = true;
+            this.txtLanguage.Size = new System.Drawing.Size(215, 20);
+            this.txtLanguage.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "OCR language";
             // 
             // OcrForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 261);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(498, 401);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "OcrForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "OCR BBY";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgInput)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox imgInput;
+        private System.Windows.Forms.Button cmdSelectImage;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button cmdBrowseTessdataDir;
+        private System.Windows.Forms.TextBox txtTessdataDir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLanguage;
     }
 }
--- OcrPoc/OcrForm.cs
+++ OcrPoc/OcrForm.cs
@@ -3,18 +3,84 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tesseract;
 
 namespace OcrPoc
 {
     public partial class OcrForm : Form
     {
+        public string TessdataDirectoryPath { get; set; }
+        public string OcrLocale { get; set; }
+
         public OcrForm()
         {
             InitializeComponent();
+        }
+
+        private void cmdSelectImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var path = dialog.FileName;
+                FileInfo file = new FileInfo(path);
+
+                string filePath = file.FullName;
+
+                imgInput.ImageLocation = filePath;
+                ReadTextFromImage(filePath);
+            }
+        }
+
+        private void ReadTextFromImage(string path)
+        {
+            if(!String.IsNullOrEmpty(this.TessdataDirectoryPath))
+            {
+                try
+                {
+                    using (var tesseract = new TesseractEngine(this.TessdataDirectoryPath, this.OcrLocale))
+                    {
+                        using (var img = Pix.LoadFromFile(path))
+                        {
+                            using (var page = tesseract.Process(img))
+                            {
+                                var text = page.GetText();
+                                txtOutput.Text = text;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "There has been an error while processing the image");
+                }
+            }
+            else
+            {
+                MessageBox.Show("tessdata path hasn't been set yet. Make sure to set it first before attempting to scan images.");
+            }
+        }
+
+        private void cmdBrowseTessdataDir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                this.TessdataDirectoryPath = dialog.FileName;
+                FileInfo file = new FileInfo(TessdataDirectoryPath);
+
+                this.TessdataDirectoryPath = file.DirectoryName;
+                this.OcrLocale = file.Name.Replace($"{file.Extension}", null);
+
+                txtTessdataDir.Text = this.TessdataDirectoryPath;
+                txtLanguage.Text = this.OcrLocale;
+            }
         }
     }
 }
